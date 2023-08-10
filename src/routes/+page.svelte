198373b6ae25bd81c07e71a3c304b37d31b3737d
hossammenem <hossammenem0102840@gmail.com>
2023-08-10 14:19:24 +0300

    trying to resovle the image flicking issue on the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,13 +14,15 @@
       css: (t: number, u:number) => `opacity: ${t}; transform: translateY(${u*2}rem);`
     };
   }
+
+  import test from "$lib/images/landingpage.svg"
 </script>
 
 {#if ready}
 <Container styles="overflow-hidden">
 <div class="grid grid-cols-9 grid-rows-2 mt-32 mb-4 p-2 gap-4" transition:fadeIn>
   <div class="sm:col-[1/6] col-[1/-1] row-span-4">
-    <img src="/landingpage.svg" alt="Finance Img" />
+    <img src={test} alt="Finance Img" />
   </div>
   <div class="text-3xl sm:text-4xl md:text-6xl sm:col-[2/-1] font-bold col-[1/-1] row-span-2">
     Sign in and start tracking your Financials Now.
